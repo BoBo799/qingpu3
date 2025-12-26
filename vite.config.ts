e78506f387a04ee448b03f9fe1c457f7411caef7
BoBo799 <58651820+BoBo799@users.noreply.github.com>
2025-12-26 23:58:15 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/power-order-demo/', // 明确设置为 GitHub 仓库名称
+      base: '/qingpu3/', // 明确设置为 GitHub 仓库名称
       server: {
         port: 3000,
         host: '0.0.0.0',
